95e19d53672c94ebad87e886d122f63130996a75
Adrian L Lange <adrianlund@gmail.com>
2013-02-03 23:35:43 +0100

    Majik

---
+++ source/context.js
@@ -0,0 +1,16 @@
+var url = 'http://www.google.com/searchbyimage?image_url=';
+
+contextMenus.create({
+	'title': 'Search Google using this image',
+	'contexts': ['image'],
+	'id': 'contextMenu',
+	'onclick': function(info, tab){
+		var src = info.srcUrl;
+		if(src.indexOf('data:') != 0){
+			chrome.tabs.create({
+				'url': url + encodeURIComponent(src)
+				'index': tab.index + 1,
+			});	
+		}
+	}
+});
