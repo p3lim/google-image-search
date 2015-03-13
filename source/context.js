e0512c6958b2dd01d7c7746f4e519a318bfc9c67
Adrian L Lange <mail@p3lim.net>
2015-03-13 01:06:54 +0100

    Make the background page nonpersistent

--- source/context.js
+++ source/context.js
@@ -1,16 +1,17 @@
 var url = 'http://www.google.com/searchbyimage?image_url=';
 
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+	var src = info.srcUrl;
+	if(src.indexOf('data:') != 0){
+		chrome.tabs.create({
+			url: url + encodeURIComponent(src),
+			index: tab.index + 1
+		})
+	}
+});
+
 chrome.contextMenus.create({
 	'title': 'Search Google using this image',
 	'contexts': ['image'],
-	'id': 'contextMenu',
-	'onclick': function(info, tab){
-		var src = info.srcUrl;
-		if(src.indexOf('data:') != 0){
-			chrome.tabs.create({
-				'url': url + encodeURIComponent(src),
-				'index': tab.index + 1,
-			});	
-		}
-	}
+	'id': 'contextMenu'
 });
