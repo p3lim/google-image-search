88d3119728eb975364e9105b105d4fea8c25e16a
Adrian L Lange <mail@p3lim.net>
2016-04-04 15:28:42 +0200

    Don't display the context menu entry on "invalid" images
    Fixes #2

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,19 +1,17 @@
 var url = 'https://www.google.com/searchbyimage?image_url=';
 
 chrome.contextMenus.onClicked.addListener(function(info, tab){
-	var src = info.srcUrl;
-	if(src.indexOf('data:') !== 0){
-		chrome.tabs.create({
-			url: url + encodeURIComponent(src),
-			index: tab.index + 1
-		})
-	}
+	chrome.tabs.create({
+		url: url + encodeURIComponent(info.srcUrl),
+		index: tab.index + 1
+	})
 });
 
 var onInitialize = function(){
 	chrome.contextMenus.create({
 		title: 'Search Google using this image',
 		contexts: ['image'],
+		targetUrlPatterns: ['https://*/*', 'http://*/*'],
 		id: 'contextMenu'
 	});
 };
